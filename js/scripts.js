b5a97d134f0a5001d5dfa28538270b036621d007
Kaveh Saleminejad, Natty Ayana and Tanner Damron <natty@>
2018-10-15 16:21:53 -0700

    add 2 more functions and clickable images

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,14 @@ $(function() {
   console.log(sentence);
   var sFirst=sentence.charAt(0);
   var sLast=sentence.charAt(sentence.length-1);
+  $(".img1").click(function() {
+  alert(sentence);});
+  $(".img2").click(function() {
+  alert(reverseAll(fourth(sFirst,sLast,sentence)))});
   console.log(uppercaseFirstAndLast(sFirst,sLast));
   console.log(changeSentence(sFirst,sLast,sentence));
+  console.log(fourth(sFirst,sLast,sentence));
+  console.log(reverseAll(fourth(sFirst,sLast,sentence)));
 });
 function uppercaseFirstAndLast(sFirst,sLast){
   sFirst= sFirst.toUpperCase();
@@ -18,6 +24,13 @@ function changeSentence(sFirst,sLast,sentence) {
   return (sentence+reverseFirstAndLast(uppercaseFirstAndLast(sFirst,sLast)));
 }
 function fourth(sFirst,sLast,sentence) {
-  var middleIndex = int(sentence.length/2);
+  var middleIndex = (sentence.length/2-(sentence.length%2)/2);
+  return  (sentence.charAt(middleIndex)+changeSentence(sFirst,sLast,sentence));
 
+}
+function reverseAll(sentence) {
+var str = sentence.split(""); //convert 'jQuery' to array
+str = str.reverse(); //reverse 'jQuery' order
+str = str.join(""); //then join the reverse order values together
+return str;
 }
